386ed191771574f961bcdfccbdc4d5216a6bc2bc
AntonNikolaev <Anton.Nikolaev.Kazan@gmail.com>
2022-12-10 19:51:43 +0400

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# TodoApp
- 
+# TodoAPI (netcoreapp3.1)
+
+- swagger
+
+- nlog
+
+- ms sql db
---
+++ Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TodoApi.DAL;
+using TodoApiDTO.BL.Services;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("TodoAPIConnection");
+
+            services.AddDbContext<TodoContext>(options =>
+                options.UseSqlServer(connection));
+
+            services.AddControllers();
+            services.AddSwaggerGen( c =>
+            {
+                var filePath = 
+                    Path.Combine(AppContext.BaseDirectory, 
+                    Configuration.GetValue<string>("SwaggerDescriptionFileXML"));
+                c.IncludeXmlComments(filePath);
+            });
+
+            services.AddScoped<ITodoService, TodoService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoAPI V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
